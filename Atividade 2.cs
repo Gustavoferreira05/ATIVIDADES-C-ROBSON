53cf122368f9b90ee15be5fae8c59061c48a0442
Gustavoferreira05 <151742846+Gustavoferreira05@users.noreply.github.com>
2025-02-21 19:44:43 -0300

    Add files via upload

---
+++ Atividade 2.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Digite um numero A:");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo numero B:");
+int n2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro numero C:");
+int n3 = int.Parse(Console.ReadLine());
+
+int R = n1 + n2;
+int SomaR = R * R;
+
+int S = n2 + n3;
+int SomaS = S * S;
+
+int SomaRS = (SomaR + SomaS) / 2;
+Console.WriteLine(SomaRS);
